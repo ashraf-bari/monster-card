1fad0c0853227fb54b046ab231f4dc48493f9222
ashraful bari <abchowdh@sfu.ca>
2020-09-26 01:12:42 -0700

    Updates

---
+++ precache-manifest.18fe50ae1ef8de2bb1ac5722c86c8541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac78a3020c99c0ca1cc2efb6940d7b49",
+    "url": "/monster-card/index.html"
+  },
+  {
+    "revision": "9d161c492e9c4124fd3a",
+    "url": "/monster-card/static/css/main.886464bc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-card/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-card/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d161c492e9c4124fd3a",
+    "url": "/monster-card/static/js/main.9e7f7a11.chunk.js"
+  },
+  {
+    "revision": "5547d39eefac2572ec28",
+    "url": "/monster-card/static/js/runtime-main.120a9896.js"
+  }
+]);
